fae0685bd98d153236a295ab9e76ee79630ab602
Diana Patron <patrondiana1309@gmail.com>
2022-08-04 23:32:33 -0500

    Add pagination class to handle api pagination

---
+++ src/pagination.py
@@ -0,0 +1,48 @@
+import math
+
+
+class Pagination:
+    def __init__(self, rows, per_page, current_page):
+        self.total_rows = rows
+        self.rows_per_page = per_page
+        self.current_page = current_page
+
+    @property
+    def current_page(self):
+        return self._current_page
+
+    @current_page.setter
+    def current_page(self, page):
+        if page < 1:
+            raise ValueError("Negative values are not allowed in page numbers")
+        self._current_page = page
+
+    @property
+    def total_pages(self):
+        return math.ceil(self.total_rows / self.rows_per_page)
+
+    @property
+    def row_offset(self):
+        return self.current_page * self.rows_per_page - self.rows_per_page
+
+    @property
+    def pages(self):
+        return range(1, self.total_pages + 1)
+
+    @property
+    def next_page(self):
+        return self._get_page_offset(1)
+
+    @property
+    def prev_page(self):
+        return self._get_page_offset(-1)
+
+    def _get_page_offset(self, offset):
+        try:
+            return self.pages[self.pages.index(self.current_page + offset)]
+        except ValueError:
+            return None
+
+    def get_metadata(self):
+        return {"total_pages": self.total_pages, "current_page": self.current_page,
+                "prev_page": self.prev_page, "next_page": self.next_page}
